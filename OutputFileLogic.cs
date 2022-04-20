b67f186b9ffab504c27447a9161019a612b6cb6b
STAT3FARM <78130608+jacobvanbeekum@users.noreply.github.com>
2022-04-20 15:59:22 -0600

    Assignment 7 complete.

--- OutputFileLogic.cs
+++ OutputFileLogic.cs
@@ -8,24 +8,100 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
 
 namespace Assignment3
 {
     internal class OutputFileLogic
     {
+        /// <summary>
+        /// Variables
+        /// </summary>
         private string outputData = "";
+        StreamWriter myWriter;
 
+        /// <summary>
+        /// This is the main constructor for the outputfilelogic class
+        /// </summary>
+        /// <param name="scoreData"></param>
         public OutputFileLogic(string scoreData)
         {
-            outputData = scoreData;
+            try
+            {
+                outputData = scoreData;
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
         }
-        
+
+        /// <summary>
+        /// This method is called by the main window to output the file.
+        /// </summary>
         public void OutputFile()
         {
-            //Simulate writing to a large file
-            Thread.Sleep(10000);
+            try
+            {
+                string sFile = @"C:\Users\Public\OutputFile.txt";
+
+                if (File.Exists(sFile))
+                {
+                    var result = MessageBox.Show("You are about to overwrite the existing file.", "", MessageBoxButtons.OKCancel);
+                    if (result == DialogResult.OK)
+                    {
+                        File.Delete(sFile);
+                        using (myWriter = new StreamWriter(sFile))
+                        {
+                            myWriter.Write(outputData);
+                            //Simulate writing to a large file
+                            Thread.Sleep(2000);
+                        }
+                    }
+                    else if (result == DialogResult.Cancel)
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    using (myWriter = new StreamWriter(sFile))
+                    {
+                        myWriter.Write(outputData);
+                        //Simulate writing to a large file
+                        Thread.Sleep(2000);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+
+
+
+
         }
 
-
+        /// <summary>
+        /// Error handling function
+        /// </summary>
+        /// <param name="sClass"></param>
+        /// <param name="sMethod"></param>
+        /// <param name="sMessage"></param>
+        private void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {
+                MessageBox.Show(sClass + "." + sMethod + " -> " + sMessage);
+            }
+            catch (System.Exception ex)
+            {
+                System.IO.File.AppendAllText(@"C:\Error.txt", Environment.NewLine + "HandleError Exception: " + ex.Message);
+            }
+        }
     }
 }
